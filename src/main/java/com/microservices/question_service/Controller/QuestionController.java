018d8a832aae4117dc43a0dae453b6da2e0fd214
vaibhavshinde7887 <vshinde7887@gmail.com>
2025-03-19 01:08:14 +0530

    Creating Question Services using microservices

---
+++ src/main/java/com/microservices/question_service/Controller/QuestionController.java
@@ -0,0 +1,38 @@
+package com.microservices.question_service.Controller;
+
+
+import com.microservices.question_service.Entity.Question;
+import com.microservices.question_service.Service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+
+@RequestMapping("question")
+public class QuestionController {
+
+    @Autowired
+    QuestionService questionService;
+
+    @GetMapping("/allQuestions")
+    public ResponseEntity<List<Question>> getAllQuestions(){
+        return questionService.getAllQuestions();
+
+    }
+
+    @GetMapping("/category/{category}")
+    public ResponseEntity<List<Question>> getQuestionsByCategory(@PathVariable String category) {
+
+        return questionService.getQuestionsByCategory(category);
+    }
+
+    @PostMapping("/addQuestion")
+    public ResponseEntity<String> addQuestion(@RequestBody Question question) {
+        return questionService.addQuestion(question);
+
+    }
+}
